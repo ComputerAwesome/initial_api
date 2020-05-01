76c31966adcd354a4dbc5c4ec8b9bd394911c728
parsercoder <parsercoder@gmail.com>
2020-05-01 23:27:04 +0200

    fixing eslit script error in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from "express"
+import express from 'express'
 
 const app = express()
 
 app.listen(process.env.PORT || 6565, () => {
-  console.log("listening to port 6565")
+  console.log('listening to port 6565')
 })
